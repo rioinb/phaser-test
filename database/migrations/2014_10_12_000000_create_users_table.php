84c8329579a79df9da6ed7253c9c3989d4b49bd4
rioinb <rio.inb@gmail.com>
2022-02-25 17:30:46 +0900

    google login

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('provider_id')->nullable();
+            $table->string('provider_token')->nullable();
+            $table->string('provider_refresh_token')->nullable();
+            $table->string('provider_name')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+
+            // 複合ユニークキー
+            $table->unique(['provider_id', 'provider_name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('twitter');
+            $table->string('password')->nullable(false)->change();
+        });
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/auth/{provider}/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name('socialOAuth');;
+
+Route::get('/auth/{provider}/callback', function ($provider) {
+    $socialUser = Socialite::driver($provider)->user();
+
+    $user = User::where('provider_id', $socialUser->id)
+        ->where('provider_name', $provider)->first();
+
+    if ($user) {
+        $user->update([
+            'provider_id' => $socialUser->id,
+            'provider_token' => $socialUser->token,
+            'provider_refresh_token' => $socialUser->refreshToken,
+        ]);
+    } else {
+        $user = User::create([
+            'name' => $socialUser->name,
+            'email' => $socialUser->email,
+            'provider_id' => $socialUser->id,
+            'provider_name' => $provider,
+            'provider_token' => $socialUser->token,
+            'provider_refresh_token' => $socialUser->refreshToken,
+        ]);
+    }
+
+    Auth::login($user);
+
+    return redirect('/');
+});
